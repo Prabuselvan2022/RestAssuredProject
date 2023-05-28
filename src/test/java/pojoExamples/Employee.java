f426c5e8bffc214382360cda8a8c511a2db0e16e
Prabuselvan2022 <prabuelamuruguselvan@gmail.com>
2023-05-28 21:47:30 +0530

    restapi

---
+++ src/test/java/pojoExamples/Employee.java
@@ -0,0 +1,11 @@
+package pojoExamples;
+
+public class Employee {
+	private int id;
+	private String name;
+	private String gender;
+	private int deptId;
+	private String mobileNum;
+	private String address;
+
+}
---
+++ src/test/java/pojoExamples/Student.java
@@ -0,0 +1,12 @@
+package pojoExamples;
+
+public class Student {
+	private int accountNo;
+	private double balance;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String gender;
+	
+
+}
